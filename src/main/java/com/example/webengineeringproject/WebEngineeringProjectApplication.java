a993c477cbbbe75d6c50fea9550d88f27ef2aa1c
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-10-05 17:24:20 +0200

    initial commit

---
+++ src/main/java/com/example/webengineeringproject/WebEngineeringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.webengineeringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEngineeringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEngineeringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webengineeringproject/WebEngineeringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webengineeringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEngineeringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
