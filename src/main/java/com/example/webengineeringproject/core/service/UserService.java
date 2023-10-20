6fbd23bac0c346195a4387aa8beea533ca94dc39
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-10-20 23:09:54 +0200

    Exceptions,UserDTO,updated User model,repository,service,controller

--- src/main/java/com/example/webengineeringproject/core/service/UserService.java
+++ src/main/java/com/example/webengineeringproject/core/service/UserService.java
@@ -2,12 +2,17 @@ package com.example.webengineeringproject.core.service;
 
 import com.example.webengineeringproject.core.api.mailsender.MailSender;
 import com.example.webengineeringproject.core.model.User;
+import com.example.webengineeringproject.core.model.enums.UserRole;
 import com.example.webengineeringproject.core.repository.UserRepository;
+import com.example.webengineeringproject.rest.dto.UserDTO;
+import com.example.webengineeringproject.rest.dto.UserRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.webengineeringproject.core.exceptions.repository.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -22,17 +27,63 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    public Optional<User> getUserById(int id) {
-        return userRepository.findById(id);
-    }
-
-    public String sendEmailToAllUsers(String message) {
+    public List<UserDTO> getAllUsers() {
         List<User> users = userRepository.findAll();
+        return users
+                .stream()
+                .map(UserDTO::new)
+                .collect(Collectors.toList());
+    }
 
-        return sendgridSender.send(users, message);
+    public UserDTO register(UserRequestDTO payload) {
+        User user = userRepository.save(payload.toEntity());
+
+        return new UserDTO(user);
+    }
+
+    public UserDTO login(String username, String password) {
+        User user = userRepository.findByUsernameAndPassword(username, password)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid username or password"));
+        return new UserDTO(user);
+    }
+
+    public UserDTO getUserById(String userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new ResourceNotFoundException("The user with the given ID does not exist.");
+        }
+        return new UserDTO(user.get());
+    }
+
+    public UserDTO updateUser(String userId, UserRequestDTO payload) {
+        Optional<User> existingUser = userRepository.findById(userId);
+        if (existingUser.isEmpty()) {
+            throw new ResourceNotFoundException("The user with the given ID does not exist.");
+        }
+        User updatedUser = payload.toEntity();
+        updatedUser.setUserId(existingUser.get().getUserId());
+        userRepository.save(updatedUser);
+        return new UserDTO(updatedUser);
+    }
+
+    public void deleteUser(String userId) {
+        Optional<User> user = userRepository.findById(userId);
+        user.ifPresent(userRepository::delete);
+    }
+
+    public void updateRole(String userId, UserRole role) {
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (userOpt.isEmpty()) {
+            throw new ResourceNotFoundException("The user with the given ID does not exist.");
+        }
+        User user = userOpt.get();
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+    public UserDTO filterByEmail(String email) {
+        Optional<User> user = userRepository.findFirstByEmailLike(email);
+
+        return user.map(UserDTO::new).orElse(null);
     }
 }
---
+++ src/main/java/com/example/webengineeringproject/rest/dto/UserRequestDTO.java
@@ -0,0 +1,62 @@
+package com.example.webengineeringproject.rest.dto;
+
+import com.example.webengineeringproject.core.model.User;
+import com.example.webengineeringproject.core.model.enums.UserRole;
+
+public class UserRequestDTO {
+    private String username;
+    private String password;
+    private String email;
+    private UserRole role;
+
+    public UserRequestDTO() { }
+
+    public UserRequestDTO(User user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+    }
+
+    public User toEntity() {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setRole(role);
+        return user;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
