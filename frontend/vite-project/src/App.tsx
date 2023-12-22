c2eace1c16c63dc692a925fb7c11add702e581c0
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-12-23 00:39:21 +0100

    Register form

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -3,11 +3,17 @@ import Home from './pages/Home';
 import LoginForm from './components/LoginForm';
 import LoginPage from './pages/LoginPage';
 import './App.css';
+// App.tsx
+import RegisterForm from './components/RegisterForm';
+
 
 
 const App = () => {
   return (
-    <LoginPage />
+
+    <div>
+    <RegisterForm />
+  </div>
   );
 };
 
