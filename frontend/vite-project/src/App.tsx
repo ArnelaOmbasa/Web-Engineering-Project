39332ccc7f642cae7dcb79c820780ffe8c8a3394
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-12-22 23:38:24 +0100

    NavBar component and HomePage

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import RecipeList from './components/RecipeList'; 
-import './App.css'; 
+import Home from './pages/Home';
+import './App.css';
 
 const App = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h1>Welcome to the Recipe Sharing Platform</h1>
-      <RecipeList />
-    </div>
+    <Home />
   );
 };
 
