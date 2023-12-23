6113edc6432f5b383c2966680c4e1cbfb553cf15
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-12-23 23:41:21 +0100

    Recipe Upload Form component

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,12 +1,28 @@
 // App.tsx
-import React from 'react';
-import UserNavBar from './components/UserNavBar'; // Adjust the path as necessary
+import React, { useState } from 'react';
+import RecipeUploadForm from './components/RecipeUploadForm'; // Adjust the import path as needed
 
 const App = () => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleOpenForm = () => setIsFormOpen(true);
+  const handleCloseForm = () => setIsFormOpen(false);
+
+  const handleUploadRecipe = (title: string, description: string, ingredients: string[], imageURL: string) => {
+    console.log('Recipe Data:', title, description, ingredients, imageURL);
+    // Integration with backend logic goes here
+    handleCloseForm(); // Close the form upon submission
+  };
+
   return (
     <div>
-      <UserNavBar />
-      {/* Other components go here */}
+      <button onClick={handleOpenForm}>Add New Recipe</button>
+      <RecipeUploadForm 
+        open={isFormOpen} 
+        onClose={handleCloseForm} 
+        onUpload={handleUploadRecipe} 
+      />
+      {/* Other components can go here */}
     </div>
   );
 };
