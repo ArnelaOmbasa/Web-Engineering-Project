13e0e0110f3ec9d018146e03490ff1bf7a5544d0
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-12-24 19:56:53 +0100

    Successfull and Error Alert components

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import UserProfile from './pages/UserProfile';
-import './App.css';
-// other imports...
+import React, { useState } from 'react';
+import SuccessAlert from './components/SuccessAlert/SuccessAlert';
+import ErrorAlert from './components/ErrorAlert/ErrorAlert';
 
-function App() {
+const App = () => {
+  const [alert, setAlert] = useState({ type: '', message: '' });
+
+  // Function to show success alert
+  const showSuccess = () => {
+    setAlert({ type: 'success', message: 'Success message!' });
+  };
+
+  // Function to show error alert
+  const showError = () => {
+    setAlert({ type: 'error', message: 'Error message!' });
+  };
+
   return (
-    <div className="App">
-      {/* other components */}
-      <UserProfile />
-      {/* other components */}
+    <div>
+      <button onClick={showSuccess}>Show Success Alert</button>
+      <button onClick={showError}>Show Error Alert</button>
+      {alert.type === 'success' && <SuccessAlert message={alert.message} />}
+      {alert.type === 'error' && <ErrorAlert message={alert.message} />}
     </div>
   );
-}
+};
 
 export default App;
