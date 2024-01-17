845cf8ebcf4f7a34ec1f675bfb012e00c008f7a2
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2024-01-17 16:17:04 +0100

    cors configuration and react-query setup

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+ });
+ 
 
 ReactDOM.render(
   <React.StrictMode>
+       <QueryClientProvider client={queryClient}>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
