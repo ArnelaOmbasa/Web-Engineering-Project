700dd9a7bdc81ea665cb401f39696655a2688dc1
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2024-01-21 00:38:04 +0100

    Adding an interceptor that adds the auth token to every request in appAxios

--- frontend/vite-project/src/services/appAxios.ts
+++ frontend/vite-project/src/services/appAxios.ts
@@ -7,5 +7,11 @@ const appAxios = axios.create({
   timeout: 10000,
 });
 
+// This is an interceptor that adds the auth token to every request
+appAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('userToken');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
 
 export default appAxios;
